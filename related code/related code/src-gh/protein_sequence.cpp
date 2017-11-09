01534df19a799552c95e168fef9c48767cd67834
liangsunatdlut <liangsun@dlut.edu.cn>
2017-11-09 13:08:39 +0800

    Revert "unzip"
    This reverts commit a86e136634d1c6e89160944d1cf7a220a4881181.

--- related code/related code/src-gh/protein_sequence.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * protein_sequence.cpp
- *
- *  Created on: 2011/04/11
- *      Author: shu
- */
-
-#include "protein_sequence.h"
--- related code/related code/src-gh/sequence.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * sequence.cpp
- *
- *  Created on: 2009/06/19
- *      Author: shu
- */
-
-#include "sequence.h"
